77905379b6654b0ce85921999f9557b79e54dc3b
Jorge Alejandro Quiroz Serna <jakop.box@gmail.com>
2019-04-10 20:53:23 -0500

    Create my first cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const toUpperCase = string => string.toUpperCase();
+
+exports.addMessage = functions.https.onRequest((request, response) => {
+    const text = request.query.text;
+    const secretText = toUpperCase(`The messge: ${text}`);
+
+    admin.database()
+        .ref('/messages/data')
+        .push({text : secretText})
+        .then(() => response.json({ message : 'great', secretText}))
+        .catch(() => response.json({message : 'Bad request!'}))
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
